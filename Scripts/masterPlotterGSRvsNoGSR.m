491f948b11318978aea9060d752b3579ca10d047
rudra101 <rudradeepmukherjee.mukherjee@gmail.com>
2021-01-19 15:20:01 +0530

    update plotting script files

--- Scripts/masterPlotterGSRvsNoGSR.m
+++ Scripts/masterPlotterGSRvsNoGSR.m
@@ -4,76 +4,82 @@ noGSRPath = '~/Downloads/Without GSR Timeseries/';
 %group = 'asd_child';
 %group = 'asd_adolsc';
 %group = 'asd_adult';
-%group = 'td_child';
-group = 'td_adolsc';
+group = 'td_child';
+%group = 'td_adolsc';
 %group = 'td_adult';
 diaggroup = group;
-%for diaggroup = ["asd_child", "asd_adolsc", "asd_adult", "td_child", "td_adolsc", "td_adult"];
+for diaggroup = ["asd_child", "asd_adolsc", "asd_adult", "td_child", "td_adolsc", "td_adult"];
 %for flag = [0 1 2]; % 0 - no GSR, 1 - GSR, 2 - Exact Ezaki.
-	sphereRadius = 10;
+        sphereRadius = 10;
         flag = 2;
 	if flag == 0; filepath = sprintf('%s%s_collated.dat',GSRPath, diaggroup)
 	elseif flag == 1; filepath = sprintf('%s%s_collated.dat',noGSRPath, diaggroup)
 	elseif flag == 2; filepath = sprintf('%s%s_collated_%dmm.dat', exactEzakiPath, diaggroup, sphereRadius) 
 	end
-%	group = convertStringsToChars(diaggroup);
+	group = convertStringsToChars(diaggroup);
 %%TBD: load data for variables instead of computing below if the file already exist.
-	nodeNumber= 7; initCondsNum=1; N = 10^5; transient = 7*10^6;
+	nodeNumber= 7; initCondsNum=1; N = 10^5; transient = 5*10^3;
 	binarizedData = importdata(filepath);
 	[h,J] = pfunc_02_Inferrer_ML(binarizedData);
 	E = mfunc_Energy(h, J);
 	[LocalMinIndex, BasinGraph, AdjacentList] = mfunc_LocalMin(nodeNumber, E);
         [probN, prob1, prob2, rD, r] = pfunc_03_Accuracy(h, J, binarizedData);
-	[basins, freq, freqTrans, freqDirectTrans, basinSizePercent, simulatedStates] = simulateAndPlotDynamics(h, J, BasinGraph, LocalMinIndex, initCondsNum, transient, N);
-	StateDurationMap = computeDurationOfStates(simulatedStates);
-
+	[basins, freq, SimulatedBasinNumber, freqTrans, freqDirectTrans, basinSizePercent, simulatedStates] = simulateAndPlotDynamics(h, J, BasinGraph, LocalMinIndex, initCondsNum, transient, N);
+%logic to find the number of major states - give a predetermined number to the function.
+	majorstateIndices = findNMajorStateIndices(2, freq)
+	StateDurationMap_MajorState = computeDurationOfStates(SimulatedBasinNumber, majorstateIndices);
+	indirectTransFreq = calculateIndirectTransFreq(SimulatedBasinNumber, majorstateIndices);
+        %majorstatesOfInterest = LocalMinIndex(majorstateIndices)
+        StateDurationMap = computeDurationOfStates(SimulatedBasinNumber, []);
 %%basin size
-	%labels = {'Basin 1', 'Basin 2'};
-	labels= {};
-	for ii = 1:length(basins);
-		labels{end + 1} = sprintf('Basin %d', ii);
-	end
-	fig = figure; bar(categorical(labels), basinSizePercent, 0.3);
-	ylabel('percentage of states belonging to a basin');
-	title('basin sizes in terms of percentage');
+%	%labels = {'Basin 1', 'Basin 2'};
+%	labels= {};
+%	for ii = 1:length(basins);
+%		labels{end + 1} = sprintf('Basin %d', ii);
+%	end
+%	fig = figure; bar(categorical(labels), basinSizePercent, 0.3);
+%	ylabel('percentage of states belonging to a basin');
+%	title('basin sizes in terms of percentage');
 	%customSaveFigure(flag, fig, group, 'basinSize');
 
 %%frequency of basin stay
-	fig = figure; bar(categorical(labels), freq, 0.3);
-	title('relative frequency of basins from random walk');
-	ylabel('frequency');
+%	fig = figure; bar(categorical(labels), freq, 0.3);
+%	title('relative frequency of basins from random walk');
+%	ylabel('frequency');
 	%customSaveFigure(flag, fig, group, 'freqStay');
 
 %%transition frequencies
-	transLabels = {};
-	transFreqs = [];
-	for ii= 1:length(basins);
-	    for jj = ii+1:length(basins);
-	      transFreqs = [transFreqs freqTrans(ii,jj)];
-	      transLabels{end+1} = sprintf('Basin %d to %d', ii,jj);
-	    end
-	end
-	fig = figure; bar(categorical(transLabels), transFreqs, 0.3);
-	%transLabels = {'Basin 1 to 2', 'Basin 2 to 1'};
-	%fig = figure; bar(categorical(transLabels), [freqTrans(1,2), freqTrans(2,1)], 0.3);
-	ylabel('Frequency'); title('Transition Frequencies');
+%	transLabels = {};
+%	transFreqs = [];
+%	for ii= 1:length(basins);
+%	    for jj = ii+1:length(basins);
+%	      transFreqs = [transFreqs freqTrans(ii,jj)];
+%	      transLabels{end+1} = sprintf('Basin %d to %d', ii,jj);
+%	    end
+%	end
+%	fig = figure; bar(categorical(transLabels), transFreqs, 0.3);
+%	%transLabels = {'Basin 1 to 2', 'Basin 2 to 1'};
+%	%fig = figure; bar(categorical(transLabels), [freqTrans(1,2), freqTrans(2,1)], 0.3);
+%	ylabel('Frequency'); title('Transition Frequencies');
 	%customSaveFigure(flag, fig, group, 'transition');
 
 %%basin duration
+	majorStateDuration = StateDurationMap_MajorState(0);
+	fprintf('Major state of %s: %f ± %f\n', group, mean(majorStateDuration), std(majorStateDuration));
 	mean_basin_dur  = zeros(1, length(basins));
 	std_basin_dur = zeros(1, length(basins));
 	basinDurations = {};
 	for ii= 1:length(basins)
-		duration = StateDurationMap(basins(ii));
+		duration = StateDurationMap(ii);
 		basinDurations{end+1} = duration;
 		mean_basin_dur(ii) = mean(duration);
 		std_basin_dur(ii) = std(duration);
 	end
-	fig = figure; hold on; bar(categorical(labels), mean_basin_dur, 0.2);
-	ylabel('duration');
-	%xCnt = (get(fig1(1),'XData') + cell2mat(get(fig1,'XOffset')));
-	hold on; errorbar([1:length(basins)]', mean_basin_dur(:), std_basin_dur(:), 'k', 'LineStyle','none');
-	title('duration from random walk');
+%	fig = figure; hold on; bar(categorical(labels), mean_basin_dur, 0.2);
+%	ylabel('duration');
+%	%xCnt = (get(fig1(1),'XData') + cell2mat(get(fig1,'XOffset')));
+%	hold on; errorbar([1:length(basins)]', mean_basin_dur(:), std_basin_dur(:), 'k', 'LineStyle','none');
+%	title('duration from random walk');
 	%customSaveFigure(flag, fig, group, 'duration');
 
 %%save every relevant variable to a .mat file
@@ -83,13 +89,13 @@ diaggroup = group;
 	elseif flag == 2; prefix = "exactEzaki";
 	end
 	filename = sprintf('%s_%dmm', strcat(prefix, "_", group, "_MEM_results"), sphereRadius);
-	save(filename, 'h', 'J', 'rD', 'r', 'probN', 'freq', 'transLabels', 'transFreqs', 'basinSizePercent', 'basinDurations','mean_basin_dur', 'std_basin_dur');
+	save(filename, 'h', 'J', 'rD', 'r', 'LocalMinIndex', 'majorstateIndices', 'majorStateDuration', 'indirectTransFreq', 'probN', 'freq', 'freqTrans', 'basinSizePercent', 'basinDurations','mean_basin_dur', 'std_basin_dur');
 %% close the figures before continuing
-%	close all;
+	close all;
 %end
-%end
+end
 
-%%summarize the results. Mostly for adding in Google slides
+%%summarize the results for custom usecases.
 %TBD: figure out if image of a table can be programitically created
 %for diaggroup = ["asd_child", "asd_adolsc", "asd_adult", "td_child", "td_adolsc", "td_adult"];
 %for flag = [0 1 2];
@@ -100,7 +106,9 @@ diaggroup = group;
 %	elseif flag == 0; prefix = "no_gsr";
 %	elseif flag == 2; prefix = "exactEzaki";
 %	end
-%        filename = strcat(prefix, "_", diaggroup, "_MEM_results")
+%        %filename = strcat(prefix, "_", diaggroup, "_MEM_results")
+%	filename = sprintf('%s_%dmm', strcat(prefix, "_", group, "_MEM_results"), sphereRadius);
+%	
 %	datastruct = load(filename);
 %	freq = datastruct.freq; basinSizePercent = datastruct.basinSizePercent;
 %	transLabels = datastruct.transLabels; transFreqs = datastruct.transFreqs;
@@ -121,6 +129,14 @@ diaggroup = group;
 %	fprintf('-------------\n')
 %end
 %end
+
+function indices = findNMajorStateIndices(N, freq)
+	if N > length(freq) || N < 0;
+	     fprintf('Give valid N. Got N = %d\n', N);
+	end
+	[sortedBySize, indexBySize] = sort(freq, 'descend');
+	indices = indexBySize(1:N);
+end
 
 function customSaveFigure(flag, fig, group, property)
 	prefix = '';
--- Scripts/plotgenerator.m
+++ Scripts/plotgenerator.m
@@ -1,34 +1,52 @@
 %%below lines generated using Python code. Didn't know how to best handle these many data points across six groups.
 %%eval can be used to mimic what is being done in Python.
-for group = ["ASD", "TD"];
-for age = ["child", "adolsc", "adult"];
-eval(sprintf("datastruct = load('exactEzaki_%s_%s_MEM_results_10mm.mat');", lower(group),age));
-for prop = ["h", "J", "rD", "r", "majorstateIndices", "majorStateDuration", "indirectTransFreq", "probN", "freq", "freqTrans", "basinSizePercent", "basinDurations","mean_basin_dur", "std_basin_dur"]; 
-eval(sprintf('%s.%s.%s = datastruct.%s;', group, age, prop, prop));
-end
-end
-end
+%for group = ["ASD", "TD"];
+%for age = ["child", "adolsc", "adult"];
+%eval(sprintf("datastruct = load('exactEzaki_%s_%s_MEM_results_10mm.mat');", lower(group),age));
+%for prop = ["h", "J", "rD", "r", "majorstateIndices", "majorStateDuration", "indirectTransFreq", "probN", "freq", "freqTrans", "basinSizePercent", "basinDurations","mean_basin_dur", "std_basin_dur"]; 
+%eval(sprintf('%s.%s.%s = datastruct.%s;', group, age, prop, prop));
+%end
+%end
+%end
+
+%%summarize the basin duration data - custom
+%fprintf('--------------------\n');
+%fprintf('Basin duration stats\n');
+%for group = ["ASD", "TD"];
+%for age = ["child", "adolsc", "adult"];
+%	fprintf('--------------------\n');
+%	fprintf('Stats for %s %s\n', group, age);
+%	eval(sprintf('majorStIndx = %s.%s.majorstateIndices', group, age));
+%	eval(sprintf('basinNum = length(%s.%s.freq);', group, age));
+%	eval(sprintf('basinDur = %s.%s.basinDurations;', group, age));
+%	for ii = 1:basinNum;
+%	eval(sprintf('mean_dur = %s.%s.mean_basin_dur(%d);', group, age, ii));
+%	eval(sprintf('std_dur = %s.%s.std_basin_dur(%d);', group, age, ii));
+%	fprintf('basin %d duration: %f ± %f\n', ii, mean_dur, std_dur);
+%	end
+%end
+%end
 
 %% plotting code begins.
 %%(1) basin size for major states
-majorStates = 1:2; labels = {};
-for state = majorStates;
-	labels{end+1} = sprintf('Major st %d', state);
-end
+%majorStates = 1:2; labels = {};
+%for state = majorStates;
+%	labels{end+1} = sprintf('Major st %d', state);
+%end
 %labels{end+1} = 'Minor st (grouped)';
-%%between age groups
+%asd_basinSzDataByAge = []; td_basinSzDataByAge = [];
+%%%between age groups
 %for age = ["child", "adolsc", "adult"];
 %	asd_minorstSize = 100 - sum(eval(sprintf('ASD.%s.basinSizePercent(ASD.%s.majorstateIndices(:))', age, age)));
 %	td_minorstSize = 100 - sum(eval(sprintf('TD.%s.basinSizePercent(TD.%s.majorstateIndices(:))', age, age)));
 %
 %	asd_basinSzData = [eval(sprintf('ASD.%s.basinSizePercent(ASD.%s.majorstateIndices(:))', age, age)) ; asd_minorstSize];
 %	td_basinSzData = [eval(sprintf('TD.%s.basinSizePercent(TD.%s.majorstateIndices(:))', age, age)); td_minorstSize];
+%%	asd_basinSzData = eval(sprintf('ASD.%s.basinSizePercent(ASD.%s.majorstateIndices(:))', age, age));
+%%	td_basinSzData = eval(sprintf('TD.%s.basinSizePercent(TD.%s.majorstateIndices(:))',age,age));
 %
-%	asd_basinSzData = eval(sprintf('ASD.%s.basinSizePercent(ASD.%s.majorstateIndices(:))', age, age));
-%	td_basinSzData = eval(sprintf('TD.%s.basinSizePercent(TD.%s.majorstateIndices(:))',age,age));
-%
-%	figure; bar(categorical(labels), [asd_basinSzData, td_basinSzData], 0.65, 'grouped');
-%	legend(['ASD' ' ' char(age)], ['TD' ' ' char(age)],'Location','best');
+%	fig = figure; bar(categorical(labels), [asd_basinSzData, td_basinSzData], 0.65, 'grouped');
+%	legend(['ASD' ' ' char(age)], ['TD' ' ' char(age)],'Location','northeast');
 %	ytickformat(gca, 'percentage');
 %	ylabel('size of brain state');
 %	if age == "child"; xlabel('children');
@@ -36,36 +54,39 @@ end
 %	elseif age == "adult"; xlabel('adult');
 %	else xlabel('Not an expected age group');
 %	end
+%	[chisquare, p] = chi2test(asd_basinSzData, td_basinSzData);
+%	title(sprintf('X^{2} = %f, p = %f', chisquare, p));
+%	%customSaveFigure(fig, ['asd_td_' char(age)], 'basin_size');
+%	close all;
+%	asd_basinSzDataByAge = [asd_basinSzDataByAge, asd_basinSzData];
+%	td_basinSzDataByAge =  [td_basinSzDataByAge, td_basinSzData];
 %end
-%%within a diagnostic group
-%for group = ["ASD", "TD"];
-%	for age = ["child", "adolsc", "adult"];
-%		a = 100 - sum(eval(sprintf('%s.%s.basinSizePercent(%s.%s.majorstateIndices(:))', group, age, group, age)));
-%		eval(sprintf('%s_minorStSize = a;', age));
-%	end
-%	basindataByAge = [];
-%	for age = ["child", "adolsc", "adult"];
-%	eval(sprintf('%s_basinSize =  [%s.%s.basinSizePercent(%s.%s.majorstateIndices(:)); %s_minorStSize];', age, group, age, group, age, age));
-%	eval(sprintf('%s_basinSize = %s.%s.basinSizePercent(%s.%s.majorstateIndices(:));', age, group, age, group, age));
-%	eval(sprintf('basindataByAge = [basindataByAge, %s_basinSize];', age));
-%	end
-%	figure; bar(categorical(labels), basindataByAge, 0.65, 'grouped');
-%	legend([char(group) ' ' 'child'], [char(group) ' ' 'adolsc'], [char(group) ' ' 'adult'], 'Location', 'best');
-%	ytickformat(gca, 'percentage');
-%	ylabel('size of brain state');
+%
+%%%within a diagnostic group. Same labels as above
+%for group = ["TD"];
+%%for group = ["ASD", "TD"];
+%  childdata   = eval(sprintf('%s_basinSzDataByAge(:,1)', lower(group)));
+%  adolscdata  = eval(sprintf('%s_basinSzDataByAge(:,2)', lower(group)));
+%  adultdata   = eval(sprintf('%s_basinSzDataByAge(:,3)', lower(group)));
+%  %first, plot between child and adolsc
+%  basinSizeComparerAndPlotter(childdata, adolscdata, 'child', 'adolsc', group, labels);
+%  %second, plot between adolsc and adult 
+%  basinSizeComparerAndPlotter(adolscdata, adultdata, 'adolsc', 'adult', group, labels);
+%  %third, plot between child and adult
+%  basinSizeComparerAndPlotter(childdata, adultdata, 'child', 'adult', group, labels);
 %end
 %%basin size of minor states (TBD)
 
 %%duration of major states
 %%between age groups
 %labels = {'children', 'adolescent', 'adult'};
-%initialisation of data fields
+%%initialisation of data fields
 %for group = ["asd", "td"];
 %  for prop = ["mean", "std"];
 %   eval(sprintf('%s_%s_dur = [];', group, prop));
 %  end
 %end
-%fill the group data
+%%fill the group data
 %for age = ["child", "adolsc", "adult"];
 %	for group = ["ASD", "TD"];
 %	stateDuration = eval(sprintf('%s.%s.majorStateDuration', group, age));
@@ -82,6 +103,13 @@ end
 %plotErrorbar(mean_seq, std_seq);
 %legend('ASD', 'TD'); ylabel('duration of major states')
 %set(gca,'xticklabel', labels);
+%apply two-sample t-test for two groups
+%for age = ["child", "adolsc", "adult"];
+% asd_data = eval(sprintf('ASD.%s.majorStateDuration', age));
+% td_data = eval(sprintf('TD.%s.majorStateDuration', age));
+% [h, p, ~, stats] = ttest2(asd_data, td_data);
+% fprintf('ASD vs TD %s. h = %f, t = %f , p = %f , df = %f\n', age, h, stats.tstat, p, stats.df);
+%end
 %hold off;
 %%% duration (major states) within a diagnostic group
 %data_mean = [asd_mean_dur ; td_mean_dur];
@@ -92,32 +120,219 @@ end
 %plotErrorbar(data_mean, data_std);
 %legend({'child', 'adolescent', 'adult'}); ylabel('duration of major states')
 %set(gca,'xticklabel', labels);
+%two-sample t-test within each diagnostic group
+%for group = ["ASD", "TD"];
+%  childdata   = eval(sprintf('%s.child.majorStateDuration', group));
+%  adolscdata  = eval(sprintf('%s.adolsc.majorStateDuration', group));
+%  adultdata   = eval(sprintf('%s.adult.majorStateDuration', group));
+%  %first, compare between child and adolsc
+%  basinDurationComparer(childdata, adolscdata, 'child', 'adolsc', group);
+%  %second, compare between adolsc and adult 
+%  basinDurationComparer(adolscdata, adultdata, 'adolsc', 'adult', group);
+%  %third, compare between child and adult
+%  basinDurationComparer(childdata, adultdata, 'child', 'adult', group);
+%end
 %%transition frequencies
 %%direct transition between major states
 %between groups
-labels = {'child', 'adolescent', 'adult'};
-%initialisation of data fields
-for group = ["asd", "td"];
-   eval(sprintf('%s_dirTrans = [];', group));
-end
-%fill the group data
-for age = ["child", "adolsc", "adult"];
-	for group = ["ASD", "TD"];
-	transFreq = eval(sprintf('%s.%s.freqTrans', group, age));
-	majorStIndices = eval(sprintf('%s.%s.majorstateIndices(:)',group, age));
-	ind1 = majorStIndices(1); ind2 = majorStIndices(2);
-	majorDirTrans = transFreq(ind1,ind2) + transFreq(ind2, ind1);
-	eval(sprintf('%s_dirTrans = [%s_dirTrans, majorDirTrans];', lower(group), lower(group)));
-	end
-end
-fig = figure; bar(categorical(labels), [asd_dirTrans(:), td_dirTrans(:)], 0.65, 'grouped');
-legend('ASD', 'TD'); ylabel('Direct transition between major states')
-set(gca,'xticklabel', labels);
-%along age
-
+%labels = {'child', 'adolescent', 'adult'};
+%%initialisation of data fields
+%for group = ["asd", "td"];
+%   eval(sprintf('%s_dirTrans = [];', group));
+%end
+%%fill the group data
+%for age = ["child", "adolsc", "adult"];
+%	for group = ["ASD", "TD"];
+%	transFreq = eval(sprintf('%s.%s.freqTrans', group, age));
+%	majorStIndices = eval(sprintf('%s.%s.majorstateIndices(:)',group, age));
+%	ind1 = majorStIndices(1); ind2 = majorStIndices(2);
+%	majorDirTrans = transFreq(ind1,ind2) + transFreq(ind2, ind1);
+%	eval(sprintf('%s_dirTrans = [%s_dirTrans, majorDirTrans];', lower(group), lower(group)));
+%	end
+%end
+%steps = 100; %because trans freq is plotted in percentage
+%fig = figure; bar(categorical(labels), steps*[asd_dirTrans(:), td_dirTrans(:)], 0.65, 'grouped');
+%legend('ASD', 'TD'); ylabel('Direct trans freq between major states')
+%set(gca,'xticklabel', labels);
+%ytickformat(gca, 'percentage');
+%run chi-square test for direct transition.
+%for ii = 1:3;
+%	n1 = asd_dirTrans(ii); n2 = td_dirTrans(ii); 
+%	%[chisquare, p] = chi2test([n1; 1-n1], [n2; 1-n2]); %unpooled
+%	n1 = n1 * steps; n2 = n2 * steps; 
+%	[chi2, p2] = chi2testSingle(n1, steps, n2, steps); %pooled
+%	%[chisquarePower, p_Power] = chi2test([n1; (10^5)-n1], [n2; (10^5)-n2]) %unpooled
+%	%fprintf('chisquare = %f, p = %f\n', chisquare, p);
+%	%fprintf('chisquare = %f, p = %f | chi2 = %f, p= %f\n', chisquare, p, chi2, p2);
+%	p2
+%	fprintf('chi2 = %f, p= %f\n', chi2, p2);
+%end
+%
+%%%along age
+%labels = {'ASD', 'TD'};
+%fig = figure; bar(steps*[asd_dirTrans; td_dirTrans], 0.65, 'grouped');
+%legend('child', 'adolescent', 'adult');
+%ytickformat(gca, 'percentage');
+%ylabel('Direct transition between major states')
+%set(gca,'xticklabel', labels); 
+%for group = ["ASD", "TD"];
+%	childdata =  eval(sprintf('%s_dirTrans(1)', lower(group)));
+%	adolscdata =  eval(sprintf('%s_dirTrans(2)', lower(group)));
+%	adultdata = eval(sprintf('%s_dirTrans(3)', lower(group)));
+%	%first, compare between child and adolsc
+%	transComparer(childdata, adolscdata, steps, 'child', 'adolsc', group);
+%	%second, compare between adolsc and adult 
+%	transComparer(adolscdata, adultdata, steps, 'adolsc', 'adult', group);
+%	%third, compare between child and adult
+%	transComparer(childdata, adultdata, steps, 'child', 'adult', group);
+%end
 %%indirect transition between multiple minor states
+%between groups
+%labels = {'child', 'adolescent', 'adult'};
+%%initialisation of data fields
+%for group = ["asd", "td"];
+%   eval(sprintf('%s_indirTrans = [];', group));
+%end
+%%%fill the group data
+%for age = ["child", "adolsc", "adult"];
+%	for group = ["ASD", "TD"];
+%	transFreq = eval(sprintf('%s.%s.indirectTransFreq', group, age));
+%	eval(sprintf('%s_indirTrans = [%s_indirTrans, transFreq];', lower(group), lower(group)));
+%	end
+%end
+%steps = 100;
+%fig = figure; bar(categorical(labels), steps*[asd_indirTrans(:), td_indirTrans(:)], 0.65, 'grouped');
+%legend('ASD', 'TD'); ylabel('Indirect trans freq between major states')
+%set(gca,'xticklabel', labels);
+%ytickformat(gca, 'percentage');
+%%run chi-square test for indirect transition.
+%for ii = 1:3;
+%	n1 = asd_indirTrans(ii); n2 = td_indirTrans(ii); 
+%	%[chisquare, p] = chi2test([n1; 1-n1], [n2; 1-n2]); %unpooled
+%	n1 = n1 * steps; n2 = n2 * steps; 
+%	[chi2, p2] = chi2testSingle(n1, steps, n2, steps); %pooled
+%	%[chisquarePower, p_Power] = chi2test([n1; (10^5)-n1], [n2; (10^5)-n2]) %unpooled
+%	%fprintf('chisquare = %f, p = %f\n', chisquare, p);
+%	%fprintf('chisquare = %f, p = %f | chi2 = %f, p= %f\n', chisquare, p, chi2, p2);
+%	p2
+%	fprintf('chi2 = %f, p= %f\n', chi2, p2);
+%end
+
+%%along age
+%labels = {'ASD', 'TD'};
+%fig = figure; bar(steps*[asd_indirTrans; td_indirTrans], 0.65, 'grouped');
+%legend('child', 'adolescent', 'adult');
+%ytickformat(gca, 'percentage');
+%ylabel('Indirect transition between major states')
+%set(gca,'xticklabel', labels); 
+%for group = ["ASD", "TD"];
+%	childdata =  eval(sprintf('%s_indirTrans(1)', lower(group)));
+%	adolscdata =  eval(sprintf('%s_indirTrans(2)', lower(group)));
+%	adultdata = eval(sprintf('%s_indirTrans(3)', lower(group)));
+%	%first, compare between child and adolsc
+%	transComparer(childdata, adolscdata, steps, 'child', 'adolsc', group);
+%	%second, compare between adolsc and adult 
+%	transComparer(adolscdata, adultdata, steps, 'adolsc', 'adult', group);
+%	%third, compare between child and adult
+%	transComparer(childdata, adultdata, steps, 'child', 'adult', group);
+%end
+
+%%empirical data measures
+%%note: please generate {asd,td}_{child,adolsc,adult}_freqs matrices where each row contain freq of {major st 1, major st2, combined minor sts} using code in binarizeCollateMultipleSiteDataForMEM.m
+%generate between groups
+%cnt = 1; %index for data
+%for age = ["child", "adolsc", "adult"];
+%	%major state1
+%	asd_data = eval(sprintf('asd_%s_freqs(:,1);', age));
+%	td_data = eval(sprintf('td_%s_freqs(:,1);', age));
+%	majorSt1 = 100*[asd_data; td_data];
+%	grp = [repmat(strcat("ASD ", age), length(asd_data), 1); repmat(strcat("TD ", age), length(td_data), 1);];
+%	boxPlotsForAppearFreq(majorSt1, grp, 'Major st #1', age, 100*asd_data, 100*td_data);
+%	%major state2
+%	asd_data = eval(sprintf('asd_%s_freqs(:,2);', age));
+%	td_data = eval(sprintf('td_%s_freqs(:,2);', age));
+%	majorSt2 = 100*[asd_data; td_data];
+%	grp = [repmat(strcat("ASD ", age), length(asd_data), 1); repmat(strcat("TD ", age), length(td_data), 1);];
+%	boxPlotsForAppearFreq(majorSt2, grp, 'Major st #2', age, 100*asd_data, 100*td_data);
+%	%combined state
+%	asd_data = eval(sprintf('asd_%s_freqs(:,3);', age));
+%	td_data = eval(sprintf('td_%s_freqs(:,3);', age));
+%	minorSt = 100*[asd_data; td_data];
+%	grp = [repmat(strcat("ASD ", age), length(asd_data), 1); repmat(strcat("TD ", age), length(td_data), 1);];
+%	boxPlotsForAppearFreq(minorSt, grp, 'Minor st (combined)', age,  100*asd_data, 100*td_data);
+%end
 
 
+%%TBD: rewrite this function to preserve beauty
+function boxPlotsForAppearFreq(data, grp, label, age, data1, data2)
+fig = figure; boxplot(data, grp);
+%set(gca, 'xticklabels', {'ASD, TD'});
+ytickformat(gca, 'percentage');
+xlabel(label); ylabel('appearance freq (empirical)');
+[h, p, ~, stats] = ttest2(data1, data2);
+ fprintf('(%s) %s (ASDvs TD). h = %f, t = %f , p = %f , df = %f\n', label, age, h, stats.tstat, p, stats.df);
+customSaveFigure(fig, ['asd_td_' label '_' char(age)] , 'empirical_freq'); 
+end
+
+function transComparer(data1, data2, steps, prop_data1, prop_data2, group)
+ [chi2square, p] = chi2testSingle(data1*steps, steps, data2*steps, steps);
+ fprintf('(%s) %s vs %s. chi2square = %f, p = %f\n', group, prop_data1, prop_data2, chi2square, p);
+end
+
+%as of now, runs t-test only. have to figure how to plot.
+function basinDurationComparer(data1, data2, prop_data1, prop_data2, group)
+ [h, p, ~, stats] = ttest2(data1, data2);
+ p
+ fprintf('(%s) %s vs %s. h = %f, t = %f , p = %f , df = %f\n', group, prop_data1, prop_data2, h, stats.tstat, p, stats.df);
+end
+
+function fig = basinSizeComparerAndPlotter(data1, data2, prop_data1, prop_data2, group, labels)
+  fig = figure; bar(categorical(labels), [data1, data2], 0.65, 'grouped');
+  ytickformat(gca, 'percentage');
+  location = 'northeast';
+  if group == "TD"; location = 'best';
+  end
+  legend([char(group) ' ' prop_data1], [char(group) ' ' prop_data2], 'Location', 'northeast');
+  xlabel([char(prop_data1) ' vs ' char(prop_data2) ' (' char(group) ')']); ylabel('size of brain state');
+  [chisquare, p] = chi2test(data1, data2);
+  title(sprintf('X^{2} = %f, p = %f', chisquare, p));
+  customSaveFigure(fig, group, [prop_data1 '_' prop_data2 '_basinSz']);
+end
+
+function customSaveFigure(fig, group, property)
+        %prefix = '(exact Ezaki)'; 
+        filename = [char(group) '_' char(property)];
+	saveas(fig, filename, 'jpg');
+end
+
+%pooled version of chi-square or z-test. Refer to NPSS' PASS documentation stored on system
+function [chi2square, p] = chi2testSingle(n1, N1, n2, N2)
+%from https://in.mathworks.com/matlabcentral/answers/96572-how-can-i-perform-a-chi-square-test-to-determine-how-statistically-different-two-proportions-are-in
+       % Pooled estimate of proportion
+       p0 = (n1+n2) / (N1+N2);
+       % Expected counts under H0 (null hypothesis)
+       n1_0 = N1 * p0;
+       n2_0 = N2 * p0;
+       % Chi-square test, by hand
+       observed = [n1 N1-n1 n2 N2-n2];
+       expected = [n1_0 N1-n1_0 n2_0 N2-n2_0];
+       chi2square = sum((observed-expected).^2 ./ expected);
+       p = 1 - chi2cdf(chi2square,1);
+end
+
+%Matlab doesn't support direct comparison of proportions, this implements chi-square test between two row or column vectors.
+% the degree of freedom is treated as len(vector) - 1.
+function [chisquare, p] = chi2test(observed_data, expected_data);  
+	obs_len = length(observed_data); exp_len = length(expected_data);
+	if obs_len ~= exp_len;
+		error(sprintf('Expected and observed data are of different length'));
+	end
+	chisquare = 0;
+	for ii = 1:obs_len;
+	chisquare = chisquare + (observed_data(ii) - expected_data(ii))^2 / expected_data(ii) ;
+	end
+	p = 1-chi2cdf(chisquare, obs_len - 1);
+end
 function plotErrorbar(main_val, err_val);
 	[ngroups, nbars] = size(main_val);
 	groupwidth = min(0.8, nbars/(nbars + 1.5));
